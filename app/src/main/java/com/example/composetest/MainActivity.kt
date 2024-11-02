94d08fa93b666c688623e8b9997111fe4008495e
MCD <debchouneharoun@gmail.com>
2024-11-02 22:15:52 +0100

    test compose

---
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.composetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetest.ui.theme.ComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+@Composable
+fun MessageCard(name: Message) {
+    Row (
+        modifier = Modifier.padding(all = 8.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(100.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+
+            Text(text = name.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = name.body)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTestTheme {
+        Surface {
+            MessageCard(
+                name = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
