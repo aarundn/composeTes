785705ab327b226f4e079ddaf496387f8e6821d0
MCD <debchouneharoun@gmail.com>
2024-12-04 14:48:08 +0100

    set home view using jetpack compose

--- challenge2/src/main/java/com/example/challenge2/MainActivity.kt
+++ challenge2/src/main/java/com/example/challenge2/MainActivity.kt
@@ -4,22 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,24 +54,150 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        SearchBar()
+                        AlignYourBodyElement(
+                            text = R.string.ab1_inversions,
+                            drawable = R.drawable.image1,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        FavoriteCollectionCard(
+                            text = R.string.ab1_inversions,
+                            drawable = R.drawable.image1,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                        AlignYourBodyRow(alignYourBodyData = alignYourBodyData)
+                    }
                 }
+
             }
         }
     }
 }
 
+data class AlignYourBodyData(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
 }
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow(alignYourBodyData = alignYourBodyData)
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid(modifier = modifier, favoriteCollectionsData = alignYourBodyData)
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier,
+    alignYourBodyData: List<AlignYourBodyData>
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+
+    ) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+
+@Composable
+fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(255.dp)
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(80.dp)
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+val alignYourBodyData = listOf(
+    AlignYourBodyData(R.drawable.image1, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image2, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image3, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image4, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image5, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image1, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image2, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image3, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image4, R.string.ab1_inversions),
+    AlignYourBodyData(R.drawable.image5, R.string.ab1_inversions)
+)
+
+
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier,
+    favoriteCollectionsData: List<AlignYourBodyData>
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text, modifier.height(80.dp))
+        }
+    }
+}
+
+data class FavoriteCollectionData(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
 
 @Preview(showBackground = true)
 @Composable
@@ -78,25 +226,67 @@ fun SearchBar(
             .heightIn(min = 56.dp)
     )
 }
-@Preview()
+
 @Composable
 fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
-            .padding(16.dp)) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            painter = painterResource(drawable),
             contentDescription = null
         )
-        Text(text = stringResource(R.string.ab1_inversions))
+        Text(
+            text = stringResource(text),
+            modifier = modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
-fun GreetingPreview() {
+fun AlignYourBodyElementPreview() {
     ComposeTestTheme {
-        Greeting("Android")
+        AlignYourBodyElement(
+            text = R.string.ab1_inversions,
+            drawable = R.drawable.image1,
+            modifier = Modifier.padding(8.dp)
+        )
     }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    ComposeTestTheme {
+        FavoriteCollectionsGrid(
+            favoriteCollectionsData = alignYourBodyData,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    ComposeTestTheme {
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow(alignYourBodyData = alignYourBodyData)
+        }
+    }
+}
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
+@Composable
+fun ScreenContentPreview() {
+    ComposeTestTheme { HomeScreen() }
 }
