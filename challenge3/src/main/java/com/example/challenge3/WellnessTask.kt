0dd6ce2b5e9d9e982c90a0c97da66354522b476e
MCD <debchouneharoun@gmail.com>
2024-12-07 15:13:27 +0100

    manage state with viewmodel and finish challenge

--- challenge3/src/main/java/com/example/challenge3/WellnessTask.kt
+++ challenge3/src/main/java/com/example/challenge3/WellnessTask.kt
@@ -1,9 +1,12 @@
 package com.example.challenge3
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 class WellnessTask(
     val id: Int,
@@ -11,4 +14,11 @@ class WellnessTask(
     initialChecked: Boolean = false
 ) {
     var checked by mutableStateOf(initialChecked)
+}
+
+@Composable
+fun df(modifier: Modifier = Modifier) {
+    Row {
+
+    }
 }
