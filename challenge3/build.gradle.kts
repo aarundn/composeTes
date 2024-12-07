a51503976485622fe122f0f3a1a0106497e8b2f0
MCD <debchouneharoun@gmail.com>
2024-12-07 11:46:28 +0100

    manage state with viewmodel and finish challenge

--- challenge3/build.gradle.kts
+++ challenge3/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //viewModel
+    val lifecycle_version = "2.8.7"
+    val arch_version = "2.2.0"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
 }
