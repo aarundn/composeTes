5746fe8be9056ca7d364d9fc8f1e2296dc612dae
MCD <debchouneharoun@gmail.com>
2024-12-09 10:42:30 +0100

    some gradient broder to image profile

--- challenge4/src/main/java/com/example/challenge4/ui/components/ReplyProfileImage.kt
+++ challenge4/src/main/java/com/example/challenge4/ui/components/ReplyProfileImage.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -18,6 +21,15 @@ fun ReplyProfileImage(
 ) {
     Image(
         modifier = modifier
+            .drawBehind {
+                val brush = Brush.radialGradient(
+                    colors = listOf(
+                        androidx.compose.ui.graphics.Color(0xFF00FF00),
+                        androidx.compose.ui.graphics.Color(0xFF0000FF)
+                    )
+                )
+                drawCircle(brush = brush, style = Stroke(width = 4.dp.toPx()))
+            }
             .size(40.dp)
             .clip(CircleShape),
         painter = painterResource(id = drawableResource),
